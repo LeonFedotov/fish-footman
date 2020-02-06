12cce276abe1509adee6d9e4903adb300eb8ccfb
Leon Fedotov <LeonFedotov@users.noreply.github.com>
2020-02-06 09:13:35 +0200

    omit state duplication log

--- src/lib.js
+++ src/lib.js
@@ -37,7 +37,7 @@ module.exports = {
     },
     state = 'pending',
     status = { name: statusName, descr: state === 'success' ? successMessage : state === 'failure' ? failureMessage : statusDescription }
-  ) => oldState === state.toUpperCase() ? context.log('Same state detected, skipping.', { number, state }) : context.github.repos.createStatus(
+  ) => oldState !== state.toUpperCase() && context.github.repos.createStatus(
     context.repo({
       context: status.name,
       sha,
